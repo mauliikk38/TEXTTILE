b0f8b30da03ac2b039aa59ebef304c8e279d0a0b
mauliikk38 <maulikkotadiya60@gmail.com>
2023-06-17 15:58:08 +0530

    Updates

---
+++ precache-manifest.407d23218551d92b7b9678703570e72c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "037583591cc2c0003fc9",
+    "url": "/TEXETILE/static/css/main.0cba8170.chunk.css"
+  },
+  {
+    "revision": "037583591cc2c0003fc9",
+    "url": "/TEXETILE/static/js/main.03758359.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/TEXETILE/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "c30ca24665940c57885d",
+    "url": "/TEXETILE/static/js/2.c30ca246.chunk.js"
+  },
+  {
+    "revision": "6d6b97f2e2838590bbd7",
+    "url": "/TEXETILE/static/js/runtime~main.6d6b97f2.js"
+  },
+  {
+    "revision": "b60df88b2500d3292d7937b7dec1dee1",
+    "url": "/TEXETILE/index.html"
+  }
+];
